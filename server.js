3559fee1f81da0c5919ed9e31f67b3548a1ec6c6
Corvax <vlvete1990@yandex.ru>
2020-06-19 14:24:06 +0300

    add http-server read index.html

--- server.js
+++ server.js
@@ -1,6 +1,17 @@
-const http = require('http');
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+const PORT = 8080;
 
-http.createServer((req, res)=> {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World!');
-}).listen(8080);
+const server = http.createServer((req, res) => {
+    res.writeHead(200, {
+        'Content-Type': 'text/html',
+        'Expires': '0'
+    });
+   
+    fs.createReadStream(path.resolve(__dirname, 'index.html')).pipe(res);
+})
+
+server.listen(PORT, '0.0.0.0', () => {
+    console.log('Server started!');
+});
