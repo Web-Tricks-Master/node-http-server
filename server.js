897c52cb694f2d61e84dc89f2dcce5df75b9d8bb
Corvax <vlvete1990@yandex.ru>
2020-06-19 13:04:11 +0300

    add hello world http-server

--- server.js
+++ server.js
@@ -1,1 +1,6 @@
-console.log("Hello Node");
+const http = require('http');
+
+http.createServer((req, res)=> {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello World!');
+}).listen(8080);
